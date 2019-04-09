821c86afedcc1048a98f41dca622b06b0b2bdb4e
Lee Yunseok <thericke00@gmail.com>
2019-04-10 07:15:39 +0900

    Added Travis

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='YGGDRASIL JIGSAW PUZZLE',
+      version='1.0',
+      description='An Independent Puzzle Game',
+      url='https://github.com/YGGDRASIL-STUDIO/YGGDRASIL-JIGSAW-PUZZLE',
+      author='YGGDRASIL STUDIO',
+      license='GNU Lesser General Public License v3.0',
+      author_email='yggdrasilgamestudio@gmail.com',
+      install_requires=['requests', ],
+      zip_safe=False
+      )
